f58e710ad7ae81fe51b3a2022fa2fb51f964fa1d
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-17 23:53:12 -0400

    adding final comments

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // connect to database
+// using dot env to hide database password with .env file that is not pushed to github
 const db = mysql.createConnection(
     {
         host: "localhost",
